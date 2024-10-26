717182ca13a2e6672cd97146b4735e799cf137e1
DELL <kalagavinay@gmail.com>
2024-10-26 12:37:28 +0530

    Anagaram Oracle Question

--- src/src/Anagrams.java
+++ src/src/Anagrams.java
@@ -1,9 +1,26 @@
 import java.util.Arrays;
+import java.util.Scanner;
 import java.util.stream.IntStream;
 
 public class Anagrams {
 
-    boolean isValidAnagram(String s1, String s2){
+    public static void main(String[] args) {
+        System.out.println("Enter the Strings");
+        Scanner sc= new Scanner(System.in);
+        String s1= sc.next();
+        String s2= sc.next();
+
+        //Anagrams anagrams= new Anagrams();
+        boolean flag=isValidAnagram(s1,s2);
+        if(flag) {
+            System.out.println("Is Anagaram");
+        }else {
+            System.out.println("Not Anagram");
+        }
+
+    }
+
+    static boolean isValidAnagram(String s1, String s2){
         char[] chars1 = s1.toCharArray();
         char[] chars2 = s2.toCharArray();
 
--- src/src/Main.java
+++ src/src/Main.java
@@ -2,17 +2,6 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Enter the Strings");
-        Scanner sc= new Scanner(System.in);
-        String s1= sc.next();
-        String s2= sc.next();
-
-        Anagrams anagrams= new Anagrams();
-        boolean flag=anagrams.isValidAnagram(s1,s2);
-        if(flag) {
-            System.out.println("Is Anagaram");
-        }else {
-            System.out.println("Not Anagram");
-        }
+        System.out.println("Hello Main");
     }
 }
